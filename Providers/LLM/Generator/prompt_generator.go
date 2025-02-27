66352e30b137b38495a8a5dcca90a1e5a5b20de8
zatekoja <zayyad.atekoja@smartsheet.com>
2025-02-27 10:06:10 -0600

    wip

--- Providers/LLM/Generator/prompt_generator.go
+++ Providers/LLM/Generator/prompt_generator.go
@@ -2,4 +2,10 @@ package Generator
 
 var PredictionMarketPrompt = `A trending topic is: %s
 Context from news: %s
-Generate a yes/no prediction poll question that is actionable (e.g., "Will [X] happen by [Y date]?")`
+Generate a yes/no prediction poll question that is actionable (e.g., "Will [X] happen by [Y date]?"), 
+make sure of these few things :
+make sure the news date isn't in the past, it needs to be a future event.
+make sure the question is clear and concise.
+make sure the question is relevant to the news.
+you can look up the news to get more context.
+`
